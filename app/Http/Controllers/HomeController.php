776232fbe24e8e6a429d17c97b1c15e7a3796eea
vennymeida <vnymeida@gmail.com>
2022-05-28 11:19:52 +0700

    Create Laravel, Boostrap template, Authentication

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    public function home()
+   {
+       return view('index');
+   }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    // public function index()
+    // {
+    //     return view('home');
+    // }
+    public function index()
+   {
+       $user = Auth::user();
+       return view('main',['user' => $user]);
+   }
+}
